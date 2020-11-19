4d79dc5f5788753ab0dcea5e0183d4ba957063b1
Haaoooooo <2770954754@qq.com>
2020-11-19 11:03:56 +0800

    '11.03'

--- js/_regist.js
+++ js/_regist.js
@@ -14,3 +14,80 @@ function send(){
     },1000)
 }
 
+//手机号
+var phoneStr = /^1[3|4|5|7|8][0-9]{9}$/;
+//用户名
+var userNameStr = /^[0-9a-zA-Z_]{4,8}$/;
+//密码
+var passwordStr = /^[0-9a-zA-Z_]{6,12}$/;
+//数字验证
+var num = /^\d[0-9]{2}\d$/g; 
+
+$('#phone').on('blur',function(){
+   if($('#phone').val() == '' || phoneStr.test($('#phone').val())==false){
+       $('#phone').next().show();
+   }else{
+       $('#phone').next().hide();
+   }
+});
+$('#img-verify').on('blur',function(){
+   if($('#img-verify').val() != 'r2b7'){
+       $('#img-verify').next().next('span').show();
+   }else{
+       $('#img-verify').next().next('span').hide();
+   }
+});
+$('#verify').on('blur',function(){
+   if($('#verify').val() == '' || num.test($('#verify').val())==false){
+       $('#verify').next().next('span').show();
+   }else{
+       $('#verify').next().next('span').hide();
+   }
+});
+
+$('#user').on('blur',function(){
+   if($('#user').val() == '' || userNameStr.test($('#user').val())==false){
+       $('#user').next().show();
+   }else{
+       $('#user').next().hide();
+   }
+});
+$('#pwd').on('blur',function(){
+   if($('#pwd').val() == '' || passwordStr.test($('#pwd').val())==false){
+       $('#pwd').next().show();
+   }else{
+       $('#pwd').next().hide();
+   }
+});
+
+$('#repwd').on('blur',function(){
+   if($('#repwd').val() != $('#pwd').val() ){
+       $('#repwd').next().show();
+   }else{
+       $('#repwd').next().hide();
+   }
+});
+
+$('#regist').on('click',function(){
+   var phoneNumber = $('#phone').val();
+   var imgCode = $('#img-verify').val();
+   var code = $('#verify').val();
+   var userName=$('#user').val();
+   var password=$('#pwd').val();
+   var rePassword=$('#repwd').val();
+   console.log(userName);
+   $.ajax({
+       type:"post",
+       url: "http://192.168.1.64:3000/users/register",
+       data: "&phone=" + phoneNumber + "&code=" + imgCode + "&username=" + userName + "&password=" + password,
+       success: function(res){
+               console.log(res);
+                if(res.status == 200){
+                    window.location.href = './index.html';
+                }else{
+                    alert(res.msg);
+                }
+               
+       }   
+   });
+});
